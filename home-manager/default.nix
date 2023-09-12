99c496535871ff686374ec73be4ceef5f3c5bd54
Jeshua Lin <jeslinmx@users.noreply.github.com>
2023-09-12 15:02:16 +0800

    added a bunch of personal programs

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -7,13 +7,33 @@
   };
   xdg.enable = true;
   home.packages = with pkgs; [
+    ### ESSENTIALS ###
     home-manager
     git
     neovim
-    vivaldi
     kitty
     starship
     unstable.chezmoi
+
+    ### GNOME ###
+    gjs
+    gnome.dconf-editor
+    gnome-extension-manager
+    gnome.file-roller
+    gnome.simple-scan
+
+    ### GRAPHICAL ###
+    vivaldi
+    ytmdesktop
+    discord
+    clapper
+    vscode
+    steam
+    prismlauncher-qt5
+    obsidian
+    keepassxc
+    telegram-desktop
+    xiphos
   ];
   # programs.starship.enable = true;
   # programs.kitty.enable = true;
