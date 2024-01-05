343abf7497e10c76af41bcfa8a646f446fe5dd55
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-01-05 10:59:06 +0800

    plymouth fully takes over boot display

--- nixos/common/plymouth.nix
+++ nixos/common/plymouth.nix
@@ -1,4 +1,9 @@
 {
-    boot.initrd.systemd.enable = true; # experimentally use systemd in stage 1, required for early plymouth
-    boot.plymouth.enable = true;
+    boot = {
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        initrd.systemd.enable = true; # experimentally use systemd in stage 1, required for early plymouth
+        kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
+        plymouth.enable = true;
+    };
 }
