573e4f488f659dd484d1b5b446bf1d9fe51698e1
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-01-09 12:13:43 +0800

    apparently cloudflare-warp works out of the box now

--- nixos/common/cloudflare-warp.nix
+++ nixos/common/cloudflare-warp.nix
@@ -1,45 +1,5 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.cloudflare-warp ];
-  systemd.packages = [ pkgs.cloudflare-warp ];
-  systemd.services.warp-svc = {
-    after = [ "network-online.target" "systemd-resolved.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      StateDirectory = "cloudflare-warp";
-      User = "warp";
-      Umask = "0077";
-      # Hardening
-      LockPersonality = true;
-      PrivateMounts = true;
-      PrivateTmp = true;
-      ProtectControlGroups = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectProc = "invisible";
-      # Leaving on strict activates warp on plus
-      ProtectSystem = "full";
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-    };
-    enable = false;
-  };
-
-  # security.pki.certificateFiles = [
-  #   fetchurl {
-  #     url = "https://developers.cloudflare.com/cloudflare-one/static/documentation/connections/Cloudflare_CA.pem";
-  #     hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-  #   }
-  # ];
-  networking.firewall.allowedUDPPorts = [ 2408 ];
-
-  users.users.warp = {
-    isSystemUser = true;
-    group = "warp";
-    description = "Cloudflare Warp user";
-    home = "/var/lib/cloudflare-warp";
-  };
-  users.groups.warp = {};
+  environment.systemPackages = [ pkgs.cloudflare-warp ]; # for warp-svc
+  systemd.packages = [ pkgs.cloudflare-warp ]; # for warp-cli
 }
