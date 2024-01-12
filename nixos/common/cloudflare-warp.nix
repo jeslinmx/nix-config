b2e4714a6cdd8f27cc4f85b9391ffd73af854878
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-01-12 17:01:12 +0800

    fixup! apparently cloudflare-warp works out of the box now

--- nixos/common/cloudflare-warp.nix
+++ nixos/common/cloudflare-warp.nix
@@ -2,4 +2,5 @@
 {
   environment.systemPackages = [ pkgs.cloudflare-warp ]; # for warp-svc
   systemd.packages = [ pkgs.cloudflare-warp ]; # for warp-cli
+  systemd.targets.multi-user.wants = [ "warp-svc.service" ];
 }
