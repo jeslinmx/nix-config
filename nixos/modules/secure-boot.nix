c93ee4ac0c8ebd3b383f0d686703f125ae54c9cc
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-04-02 18:50:38 +0800

    fixed typo in secure-boot

--- nixos/modules/secure-boot.nix
+++ nixos/modules/secure-boot.nix
@@ -3,7 +3,7 @@
   # 0. reboot into UEFI menu and set Secure Boot to Setup Mode
   # 1. create signing keys using `nix run nixpkgs#sbctl create-keys`
   # 1a. alternatively, import them using `nix run nixpkgs#sbctl import-keys -- --directory ...`
-  # 2. enroll keys using `nix run nixpkgs#sbctl enroll-keys -- --append --microsoft --ignore-immutable`
+  # 2. enroll keys using `nix run nixpkgs#sbctl -- enroll-keys --append --microsoft --ignore-immutable`
   # 3. set Secure Boot back to Deployed Mode if not automatically reverted
 
   boot = {
