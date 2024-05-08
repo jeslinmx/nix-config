a69b23f3c16c6a5c359371301cffbe53fbad4875
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-05-09 03:27:02 +0800

    change flake.nix to numtide's flake-parts and devshell

--- flake.nix
+++ flake.nix
@@ -2,59 +2,76 @@
   description = "NixOS (and home-manager) configuration";
 
   inputs = {
+    # flake helpers
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+
+    # nixpkgs
+    nixpkgs.url = "nixpkgs";
+    nixpkgs-patched.url = "github:jeslinmx/nixpkgs/patch-1";
     nixos-unstable.url = "nixpkgs/nixos-unstable";
     nixos-2311.url = "nixpkgs/nixos-23.11";
-    nixpkgs-patched.url = "github:jeslinmx/nixpkgs/patch-1";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    # NixOS modules
     lanzaboote.url = "github:nix-community/lanzaboote";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nix-colors.url = "github:misterio77/nix-colors";
     home-manager-unstable.url = "github:nix-community/home-manager";
     home-manager-2311.url = "github:nix-community/home-manager/release-23.11";
-    private-config.url = "git+ssh://git@github.com/jeslinmx/nix-private-config";
-    nix-colors.url = "github:misterio77/nix-colors";
-    home-configs = {
-      url = "path:./home-manager";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager-unstable.follows = "home-manager-unstable";
-        home-manager-2311.follows = "home-manager-2311";
-        private-config.follows = "private-config";
-        nix-colors.follows = "nix-colors";
-      };
+    home-configs.url = "path:./home-manager";
+    home-configs.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      home-manager-unstable.follows = "home-manager-unstable";
+      home-manager-2311.follows = "home-manager-2311";
+      private-config.follows = "private-config";
+      nix-colors.follows = "nix-colors";
     };
+    private-config.url = "git+ssh://git@github.com/jeslinmx/nix-private-config";
   };
 
   outputs = {
-    nixpkgs,
-    flake-utils,
+    flake-parts,
+    devshell,
     home-configs,
     ...
-  } @ inputs:
-    rec {
+  } @ inputs: flake-parts.lib.mkFlake { inherit inputs; } ( { self, lib, ... }: {
+    imports = [
+      devshell.flakeModule
+    ];
+
+    flake = {
       inherit (home-configs.outputs) homeModules homeConfigurations;
-      nixosModules = with builtins; with nixpkgs.lib; let
+      nixosModules = with builtins; with lib; let
         dir = ./nixos/modules;
         dirContents = readDir dir;
         moduleFiles = filterAttrs (fname: type: (type == "regular") && (strings.hasSuffix ".nix" fname)) dirContents;
       in
-        mapAttrs' (
-          fname: _:
-            attrsets.nameValuePair
-            (strings.removeSuffix ".nix" fname)
-            (import /${dir}/${fname})
+      mapAttrs' (
+        fname: _:
+        attrsets.nameValuePair
+        (strings.removeSuffix ".nix" fname)
+        (import /${dir}/${fname})
         )
         moduleFiles;
-      nixosConfigurations = with builtins; with nixpkgs.lib; let
-        dir = ./nixos/systems;
-        dirContents = readDir dir;
-        configFiles = filterAttrs (fname: type: type == "directory") dirContents;
-      in
-        mapAttrs (fname: _: (import /${dir}/${fname}) (inputs // {inherit nixosModules;})) configFiles;
-    }
-    // flake-utils.lib.eachDefaultSystem (system: {
-      formatter = nixpkgs.legacyPackages.${system}.alejandra;
-      devShells.default = with (import nixpkgs { inherit system; }); mkShell {
-        packages = [ just alejandra nix-output-monitor nix-tree ];
+        nixosConfigurations = with builtins; with lib; let
+          dir = ./nixos/systems;
+          dirContents = readDir dir;
+          configFiles = filterAttrs (fname: type: type == "directory") dirContents;
+        in
+        mapAttrs (fname: _: (import /${dir}/${fname}) (inputs // {inherit (self) nixosModules;})) configFiles;
+    };
+
+    systems = [ "x86_64-linux" ];
+    perSystem = { system, pkgs, ... }: {
+      formatter = pkgs.alejandra;
+      devshells.default = {
+        commands = [
+          { package = pkgs.nurl; category = "dev"; }
+          { package = pkgs.nh; category = "build"; }
+          { package = pkgs.nix-tree; category = "debug"; }
+          { package = pkgs.nix-melt; category = "debug"; }
+        ];
       };
-    });
+    };
+  });
 }
