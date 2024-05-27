81654a299d7620c9edc68655286a74745424c6f0
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-05-28 07:08:31 +0800

    fixed nixPath

--- nixos/modules/nix-enable-flakes.nix
+++ nixos/modules/nix-enable-flakes.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.nixPath = [ "nixpkgs=flake" ];
   environment.systemPackages = [pkgs.git]; # required for flakes
 }
