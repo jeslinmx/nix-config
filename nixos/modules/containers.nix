d66881095374da37282ba29bdac3d1693f999abe
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-07-07 17:06:35 +0800

    podman fixes

--- nixos/modules/containers.nix
+++ nixos/modules/containers.nix
@@ -1,8 +1,9 @@
-{ lib, config, ... }: {
+{ lib, pkgs, config, ... }: {
   virtualisation = {
     containers.enable = true;
     podman = {
       enable = true;
+      enableNvidia = true;
       dockerCompat = true; # docker command alias
       dockerSocket.enable = true; # docker api socket
       defaultNetwork.settings.dns_enabled = true; # inter-container networking
@@ -11,6 +12,7 @@
         dates = "weekly";
       };
     };
-    containers.cdi.dynamic.nvidia.enable = lib.mkDefault (builtins.elem "nvidia" config.services.xserver.videoDrivers);
   };
+  hardware.nvidia-container-toolkit.enable = lib.mkDefault (builtins.elem "nvidia" config.services.xserver.videoDrivers);
+  environment.systemPackages= [ pkgs.podman-compose ];
 }
