071c2ba0da05fd9065a95113bf83fd401a29d298
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-09-06 12:07:53 +0800

    nixvim: switched to startify

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -1,9 +1,15 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   home.packages = [ pkgs.zig ];
   programs.nixvim = let
     inherit (config.nixvim) helpers;
   in {
-    autoCmd = [];
+    autoCmd = [
+      {
+        event = "User";
+        pattern = "StartifyReady";
+        command = "lua vim.b.miniindentscope_disable = true";
+      }
+    ];
     autoGroups = {};
     clipboard.register = "unnamedplus";
     editorconfig.enable = true;
@@ -29,7 +35,7 @@
       { key = "<Tab>"; action = "<Cmd>BufferLineCycleNext<CR>"; }
       { key = "<S-Tab>"; action = "<Cmd>BufferLineCyclePrev<CR>"; }
 
-      { key = "<Leader>~"; action = "<Cmd>Alpha<CR>"; options.desc = "Open start screen (Alpha)"; }
+      { key = "<Leader>~"; action = "<Cmd>Startify<CR>"; options.desc = "Open start screen (Startify)"; }
 
       ( group "<Leader><Leader>" "+scope" )
       { key = "<Leader><Leader>a"; action = "<Cmd>Telescope autocommands<CR>"; options.desc = "autocommands"; }
@@ -165,10 +171,6 @@
       gitsigns = {}; # gutter, blame, hunk actions
 
       # UI
-      alpha = { # start screen
-        enable = true;
-        theme = "startify";
-      };
       barbecue = { # context breadcrumbs
         enable = true;
         showBasename = false;
@@ -261,6 +263,55 @@
           buf_options = {
             buflisted = true;
           };
+        };
+      };
+      startify = {
+        enable = true;
+        settings = {
+          custom_header = [
+            "         oo                   oo            "
+            "                                            "
+            "88d888b. dP dP.  .dP dP   .dP dP 88d8b.d8b. "
+            "88'  `88 88  `8bd8'  88   d8' 88 88'`88'`88 "
+            "88    88 88  .d88b.  88 .88'  88 88  88  88 "
+            "dP    dP dP dP'  `dP 8888P'   dP dP  dP  dP "
+          ];
+          custom_footer = [
+            (helpers.mkRaw "tostring(math.randomseed(os.time()) or '')")
+            (helpers.mkRaw "vim.fn.system('${pkgs.kjv}/bin/kjv Psalms 119:' .. math.ceil(math.random() * 176)):match('^(.-)%s*$')")
+          ];
+          change_to_vcs_root = true;
+          custom_indices = ["a" "s" "d" "f" "g" "h" "j" "k" "l" ";"];
+          relative_path = true;
+          session_persistence = true;
+          session_sort = true;
+          update_oldfiles = true;
+          lists = [
+            {
+              type = "dir";
+              header = [
+                (helpers.mkRaw ''
+                  table.concat({
+                    '   MRU',
+                    vim.loop.cwd(),
+                    vim.fn.system('${pkgs.git}/bin/git log -1 --format="(%h %s)" -z 2> /dev/null')
+                  }, ' ')
+                '')
+              ];
+            }
+            {
+              type = "sessions";
+              header = ["    Sessions"];
+            }
+            {
+              type = "bookmarks";
+              header = ["    Bookmarks"];
+            }
+            {
+              type = "commands";
+              header = ["    Commands"];
+            }
+          ];
         };
       };
       statuscol = { enable = true; };
