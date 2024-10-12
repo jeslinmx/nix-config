bec99eacdb041705f01bf8bd88a08cd677fd79bc
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-10-13 00:27:42 +0800

    feat(lazygit): conventional commits custom command

--- home-manager/modules/lazygit.nix
+++ home-manager/modules/lazygit.nix
@@ -26,6 +26,57 @@
       };
       notARepository = "skip";
       promptToReturnFromSubprocess = false;
+      customCommands = [
+        {
+          # retrieved from: https://github.com/jesseduffield/lazygit/wiki/Custom-Commands-Compendium#conventional-commit
+          key = "c";
+          context = "files";
+          description = "Create new conventional commit";
+          prompts = [
+            {
+              type = "menu";
+              key = "Type";
+              title = "Type of change";
+              options = builtins.attrValues (builtins.mapAttrs (value: description: { inherit value description; }) {
+                build = "Changes that affect the build system or external dependencies";
+                feat = "A new feature";
+                fix = "A bug fix";
+                chore = "Other changes that don't modify src or test files";
+                ci = "Changes to CI configuration files and scripts";
+                docs = "Documentation only changes";
+                perf = "A code change that improves performance";
+                refactor = "A code change that neither fixes a bug nor adds a feature";
+                revert = "Reverts a previous commit";
+                style = "Changes that do not affect the meaning of the code";
+                test = "Adding missing tests or correcting existing tests";
+              });
+            }
+            {
+              type = "input";
+              title = "Scope";
+              key = "Scope";
+              initialValue = "";
+            }
+            {
+              type = "menu";
+              key = "Breaking";
+              title = "Breaking change?";
+              options = [
+                { name = "no"; value = ""; }
+                { name = "yes"; value = "!"; }
+              ];
+            }
+            {
+              type = "input";
+              title = "{{.Form.Type}}{{ if .Form.Scope }}({{ .Form.Scope }}){{ end }}{{.Form.Breaking}}: ...";
+              key = "Message";
+              initialValue = "";
+            }
+          ];
+          command = "git commit --message '{{.Form.Type}}{{ if .Form.Scope }}({{ .Form.Scope }}){{ end }}{{.Form.Breaking}}: {{.Form.Message}}'";
+          loadingText = "Creating conventional commit...";
+        }
+      ];
     };
   };
 }
