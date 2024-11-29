b154c6e679a8a45affb82e1974e096256a5ec9fb
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-11-29 13:19:20 +0800

    fix(nixvim): removed permissions col from oil

--- home-manager/modules/nixvim/plugins/ui/oil.nix
+++ home-manager/modules/nixvim/plugins/ui/oil.nix
@@ -2,7 +2,7 @@
   programs.nixvim.plugins.oil = {
     enable = true;
     settings = {
-      columns = [ "permissions" "size" "mtime" "icon" ];
+      columns = [ "size" "mtime" "icon" ];
       buf_options = {
         buflisted = true;
       };
