7ffc9f58cd219b46ee1ba6036dfa387f2721c433
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-12-11 15:23:27 +0800

    feat(app-server): caddy proxy

--- secrets.nix
+++ secrets.nix
@@ -5,4 +5,5 @@ let
   app-server = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmKso0pvOLWk+vJ/5gBgyVC+G8FPy+J06MUKT7rUNZ root@apps" ];
 in {
   "zt_token.age".publicKeys = jeslinmx ++ app-server;
+  "cf_token.age".publicKeys = jeslinmx ++ app-server;
 }
