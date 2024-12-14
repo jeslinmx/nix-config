6108d2abfe626784c003cd3fea3db100dc397cd2
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-12-14 18:26:27 +0800

    feat(hyprland): switch to uwsm, enabled greetd, fixed ssh-agent

--- nixos/modules/interactive/hyprland.nix
+++ nixos/modules/interactive/hyprland.nix
@@ -1,18 +1,27 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs = {
     hyprland = {
       enable = true;
-      systemd.setPath.enable = true;
+      withUWSM = true;
     };
     hyprlock.enable = true;
-    nm-applet = {
-      enable = true;
-      indicator = false;
-    };
   };
   networking.networkmanager.enable = true;
   services.logind = {
     powerKey = "hibernate";
     lidSwitchDocked = "suspend";
+  };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet ${builtins.concatStringsSep " " [
+        "--issue" # display /etc/issue
+        "--time" # display current time
+        "--user-menu" # display menu of users
+        "--asterisks" # show feedback when entering password
+        "--window-padding=1" # add some padding to the border of the screen
+        "--remember --remember-user-session" # remember last selected user and their selected session
+      ]}";
+    };
   };
 }
