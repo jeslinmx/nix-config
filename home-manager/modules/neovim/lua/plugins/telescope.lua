914337700b6e38b06e65b66be62fa1893ecf1172
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-01-07 12:11:30 +0800

    feat(neovim): telescope ui tweaks

--- home-manager/modules/neovim/lua/plugins/telescope.lua
+++ home-manager/modules/neovim/lua/plugins/telescope.lua
@@ -2,7 +2,32 @@ return { "nvim-telescope/telescope.nvim",
   dependencies = { "2kabhishek/nerdy.nvim", "debugloop/telescope-undo.nvim" },
 
   opts = {
-    path_display = "filename_first",
+    defaults = {
+      layout_strategy = "flex",
+      layout_config = {
+        prompt_position = "top",
+        horizontal = {
+          preview_width = 0.6,
+        },
+        vertical = {
+          width = 0.999,
+          preview_height = 0.6,
+          mirror = true,
+        },
+      },
+      sorting_strategy = "ascending",
+      path_display = {
+        "filename_first",
+        -- shorten = { len = 2, exclude = {1, -2, -1} },
+        "truncate",
+      },
+      mappings = {
+        i = {
+          ["<C-Down>"] = require('telescope.actions').cycle_history_next,
+          ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
+        },
+      },
+    },
   },
   config = function(_, opts)
     require("telescope").setup(opts)
