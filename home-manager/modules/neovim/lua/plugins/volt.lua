0b9197020d3ee4e977ad98ea8ca79cad93c95e02
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-01-08 18:57:25 +0800

    feat(neovim): showkeys, timerly, minty

---
+++ home-manager/modules/neovim/lua/plugins/volt.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvzone/showkeys",
+    opts = { position = "top-right", maxkeys = 5, show_count = true, excluded_modes = { "i", "c" } },
+    cmd = "ShowkeysToggle",
+    event = "UIEnter",
+  },
+  { "nvzone/minty", dependencies = "nvzone/volt", cmd = { "Shades", "Huefy" } },
+  { "nvzone/timerly", dependencies = "nvzone/volt", cmd = "TimerlyToggle" },
+}
