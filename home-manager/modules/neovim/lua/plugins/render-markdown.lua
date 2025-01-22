b8754969e6fce6c9209fc3d8c5ceb6fe4596158e
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-01-22 17:44:40 +0800

    fix(neovim): further render-markdown improvements

--- home-manager/modules/neovim/lua/plugins/render-markdown.lua
+++ home-manager/modules/neovim/lua/plugins/render-markdown.lua
@@ -2,27 +2,34 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
 
-  opts = {
-    heading = {
-      icons = {},
-      signs = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-      width = "block",
-      left_margin = 1,
-      left_pad = 2,
-      right_pad = 2,
-      border = true,
-    },
-    code = {
-      position = "right",
-      width = "block",
-      min_width = 50,
-      left_margin = 1,
-      left_pad = 2,
-      right_pad = 2,
-      inline_pad = 1,
-      border = "thick",
-    },
-  },
+  config = function(opts)
+    require("render-markdown").setup {
+      render_modes = { "n", "c", "t", "i" },
+      heading = {
+        icons = {},
+        signs = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        width = "block",
+        left_margin = 1,
+        left_pad = 2,
+        right_pad = 2,
+        border = true,
+      },
+      code = {
+        position = "right",
+        width = "block",
+        min_width = 50,
+        left_margin = 1,
+        left_pad = 2,
+        right_pad = 2,
+        inline_pad = 1,
+        border = "thick",
+      },
+      pipe_table = {
+        preset = "round",
+      },
+    }
+    require("render-markdown.integ.coq").setup()
+  end,
 
   ft = { "markdown", "latex", "html" },
 }
