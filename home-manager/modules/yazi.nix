37969e1f84231829ea005416b933de11e58172b1
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-01-31 22:43:25 +0800

    feat(yazi): more plugins

--- home-manager/modules/yazi.nix
+++ home-manager/modules/yazi.nix
@@ -33,9 +33,29 @@ in {
             desc = "Toggle preview pane maximized";
           }
           {
+            on = ["g" "s"];
+            run = "plugin what-size";
+            desc = "Calculate size of selection or current directory";
+          }
+          {
             on = "F";
             run = "plugin smart-filter";
             desc = "Filter smartly";
+          }
+          {
+            on = "l";
+            run = "plugin smart-enter";
+            desc = "Enter the child directory, or open the file";
+          }
+          {
+            on = "<Enter>";
+            run = "plugin smart-enter";
+            desc = "Enter the child directory, or open the file";
+          }
+          {
+            on = "<Right>";
+            run = "plugin smart-enter";
+            desc = "Enter the child directory, or open the file";
           }
         ];
         append_keymap = [
@@ -44,22 +64,105 @@ in {
             run = ''shell '${lib.getExe pkgs.xdragon} --on-top --and-exit --all $@' --confirm '';
             desc = "Drag and drop";
           }
+          {
+            on = ["C" "z"];
+            run = "plugin ouch --args=zip";
+            desc = "Compress into .zip";
+          }
+          {
+            on = ["C" "t"];
+            run = "plugin ouch --args=tar";
+            desc = "Compress into .tar";
+          }
+          {
+            on = ["C" "g"];
+            run = "plugin ouch --args=tar.gz";
+            desc = "Compress into .tar.gz";
+          }
+          {
+            on = ["C" "7"];
+            run = "plugin ouch --args=7z";
+            desc = "Compress into .7z";
+          }
+          {
+            on = ["C" "x"];
+            run = "plugin ouch --args=tar.xz";
+            desc = "Compress into .tar.xz";
+          }
+          {
+            on = ["C" "b"];
+            run = "plugin ouch --args=tar.bz2";
+            desc = "Compress into .bz2";
+          }
+          {
+            on = ["C" "l"];
+            run = "plugin ouch --args=tar.lz4";
+            desc = "Compress into .lz4";
+          }
         ];
       };
     };
     settings = {
-      plugin.prepend_fetchers = [
-        {
-          id = "git";
-          name = "*";
-          run = "git";
-        }
-        {
-          id = "git";
-          name = "*/";
-          run = "git";
-        }
-      ];
+      plugin = {
+        prepend_fetchers = [
+          {
+            id = "git";
+            name = "*";
+            run = "git";
+          }
+          {
+            id = "git";
+            name = "*/";
+            run = "git";
+          }
+        ];
+        append_previewers = [
+          {
+            name = "*.csv";
+            run = "rich-preview";
+          }
+          {
+            name = "*.md";
+            run = "rich-preview";
+          }
+          {
+            name = "*.rst";
+            run = "rich-preview";
+          }
+          {
+            name = "*.ipynb";
+            run = "rich-preview";
+          }
+          {
+            name = "*.json";
+            run = "rich-preview";
+          }
+          {
+            name = "*.zip";
+            run = "ouch";
+          }
+          {
+            name = "*.7z";
+            run = "ouch";
+          }
+          {
+            name = "*.tar";
+            run = "ouch";
+          }
+          {
+            name = "*.tar.*";
+            run = "ouch";
+          }
+          {
+            name = "*.rar";
+            run = "ouch";
+          }
+          {
+            name = "*";
+            run = "hexyl";
+          }
+        ];
+      };
       preview = {
         max-height = 1000;
         max-width = 1000;
@@ -70,15 +173,9 @@ in {
         owner = "yazi-rs";
         repo = "plugins";
         rev = "main";
-        hash = "sha256-Sd3sA64j2TwRuOcH14ouC4LD14sGGaxfyq9Ys4xivvM=";
+        hash = "sha256-xY2yVCLLcXRyFfnmyP6h5Fw+4kwOZhEOCWVZrRwXnTA=";
       };
     in {
-      starship = pkgs.fetchFromGitHub {
-        owner = "Rolv-Apneseth";
-        repo = "starship.yazi";
-        rev = "main";
-        hash = "sha256-wESy7lFWan/jTYgtKGQ3lfK69SnDZ+kDx4K1NfY4xf4=";
-      };
       chmod = builtins.path {path = "${yazi-rs}/chmod.yazi";};
       diff = builtins.path {path = "${yazi-rs}/diff.yazi";};
       full-border = builtins.path {path = "${yazi-rs}/full-border.yazi";};
@@ -86,11 +183,40 @@ in {
       hide-preview = builtins.path {path = "${yazi-rs}/hide-preview.yazi";};
       max-preview = builtins.path {path = "${yazi-rs}/max-preview.yazi";};
       smart-filter = builtins.path {path = "${yazi-rs}/smart-filter.yazi";};
+      smart-enter = builtins.path {path = "${yazi-rs}/smart-enter.yazi";};
+      hexyl = pkgs.fetchFromGitHub {
+        owner = "Reledia";
+        repo = "hexyl.yazi";
+        rev = "main";
+        hash = "sha256-nsnnL3GluKk/p1dQZTZ/RwQPlAmTBu9mQzHz1g7K0Ww=";
+      };
+      ouch = pkgs.fetchFromGitHub {
+        owner = "ndtoan96";
+        repo = "ouch.yazi";
+        rev = "main";
+        hash = "sha256-zLAaJrcZGNWlG2HjsZtN4u8JZAN+GLl2RtP9qCt3T74=";
+      };
+      rich-preview = pkgs.fetchFromGitHub {
+        owner = "AnirudhG07";
+        repo = "rich-preview.yazi";
+        rev = "main";
+        hash = "sha256-TwL0gIcDhp0hMnC4dPqaVWIXhghy977DmZ+yPoF/N98=";
+      };
+      what-size = pkgs.fetchFromGitHub {
+        owner = "pirafrank";
+        repo = "what-size.yazi";
+        rev = "main";
+        hash = "sha256-SDObD22u2XYF2BYKsdw9ZM+yJLH9xYTwSFRWIwMCi08=";
+      };
     };
     initLua = ''
-      -- require("starship"):setup()
       require("git"):setup()
       require("full-border"):setup { type = ui.Border.ROUNDED }
     '';
   };
+  home.packages = [
+    pkgs.hexyl
+    pkgs.ouch
+    pkgs.rich-cli
+  ];
 }
