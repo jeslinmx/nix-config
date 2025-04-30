6d73d1ec12f0816757705ae900be7f3bddf231b0
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-05-01 03:24:03 +0800

    fix(app-server): expose jellyfin http ports locally

--- modules/nixos/server/jellyfin.nix
+++ modules/nixos/server/jellyfin.nix
@@ -3,7 +3,10 @@
     jellyfin.enable = true;
     caddy.proxiedServices."jf.zt.jesl.in" = "127.0.0.1:8096";
   };
-  networking.firewall.allowedUDPPorts = [1900 7359];
+  networking.firewall = {
+    allowedTCPPorts = [8096 8920]; # for locally discovered LAN
+    allowedUDPPorts = [1900 7359];
+  };
   backups.restic.services.jellyfin = {
     paths = [config.services.jellyfin.dataDir];
     backupPrepareCommand = "systemctl stop jellyfin.service";
