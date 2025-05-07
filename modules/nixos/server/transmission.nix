5a80f3696c10c282263f8b502c5f45a2ae44dea6
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-05-07 16:00:52 +0800

    feat(app-server): replaced flood-for-transmission with shift

--- modules/nixos/server/transmission.nix
+++ modules/nixos/server/transmission.nix
@@ -6,9 +6,15 @@
   services = {
     transmission = {
       enable = true;
-      webHome = pkgs.flood-for-transmission;
+      webHome = pkgs.fetchFromGitHub {
+        owner = "killemov";
+        repo = "Shift";
+        rev = "9512734c87bc8eb1db25d8685a7a567c7a3af99e";
+        hash = "sha256-BUuc3cvW5Tr8lsuyh1OOSRFdNxhB4e0M7Ts2FW99lgI=";
+      };
       settings = {
         download-dir = "/mnt/medialibrary";
+        incomplete-dir = "/mnt/medialibrary/.incomplete";
         rpc-port = 9091;
       };
     };
