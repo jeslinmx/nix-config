95be5a5549c2bb5422ced17ebdb31a0c996e1ab2
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-05-30 23:48:38 +0800

    fix(cli-programs): yazi updated

--- modules/home-manager/yazi.nix
+++ modules/home-manager/yazi.nix
@@ -5,38 +5,96 @@
 }: {
   programs.yazi = {
     enable = true;
-    keymap = {
-      manager = {
-        prepend_keymap = [
+    plugins = {
+      inherit
+        (pkgs.yaziPlugins)
+        chmod
+        diff
+        full-border
+        git
+        mime-ext
+        mount
+        piper
+        smart-enter
+        smart-filter
+        toggle-pane
+        ouch
+        ;
+    };
+    settings = {
+      mgr = {
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_dir_first = true;
+        linemode = "mtime";
+        show_symlink = true;
+        scrolloff = 20;
+      };
+      preview = {
+        image_delay = 50;
+        max-height = 1000;
+        max-width = 1000;
+      };
+      plugin = {
+        prepend_fetchers = [
           {
-            on = ["g" "d"];
-            run = "plugin diff";
-            desc = "Diff selected file with hovered file";
+            id = "git";
+            name = "*";
+            run = "git";
           }
+          {
+            id = "git";
+            name = "*/";
+            run = "git";
+          }
+          {
+            id = "mime";
+            name = "*";
+            run = "mime-ext";
+            prio = "high";
+          }
+        ];
+        append_previewers =
+          lib.pipe {
+            "*.json" = ''piper -- ${lib.getExe pkgs.gojq} "$1"'';
+            "*.tar*" = ''piper --format=url -- ${lib.getExe pkgs.ouch} list "$1"'';
+            "*.zip" = ''piper --format=url -- ${lib.getExe pkgs.ouch} list "$1"'';
+            "*.7z" = ''piper --format=url -- ${lib.getExe pkgs.ouch} list "$1"'';
+            "*.rar" = ''piper --format=url -- ${lib.getExe pkgs.ouch} list "$1"'';
+            "*.md" = ''piper -- CLICOLOR_FORCE=1 ${lib.getExe pkgs.glow} -w=$w "$1"'';
+            "*.csv" = ''piper -- ${lib.getExe' pkgs.rich-cli "rich"} "$1"'';
+            "*.rst" = ''piper -- ${lib.getExe' pkgs.rich-cli "rich"} "$1"'';
+            "*.ipynb" = ''piper -- ${lib.getExe' pkgs.rich-cli "rich"} "$1"'';
+            "*" = ''piper -- ${lib.getExe pkgs.bat} --force-colorization --style=changes,numbers "$1"'';
+          } [
+            lib.attrsToList
+            (builtins.map ({
+              name,
+              value,
+            }: {
+              inherit name;
+              run = value;
+            }))
+          ];
+      };
+    };
+    keymap = {
+      mgr = {
+        prepend_keymap = [
           {
             on = ["g" "m"];
             run = "plugin chmod";
             desc = "Chmod selected files";
           }
           {
-            on = ["g" "t"];
-            run = "plugin hide-preview";
-            desc = "Toggle preview pane";
+            on = ["g" "d"];
+            run = "plugin diff";
+            desc = "Diff selected file with hovered file";
           }
           {
-            on = ["g" "T"];
-            run = "plugin max-preview";
-            desc = "Toggle preview pane maximized";
-          }
-          {
-            on = ["g" "s"];
-            run = "plugin what-size";
-            desc = "Calculate size of selection or current directory";
-          }
-          {
-            on = "F";
-            run = "plugin smart-filter";
-            desc = "Filter smartly";
+            on = ["g" "M"];
+            run = "plugin mount";
+            desc = "Open mount manager";
           }
           {
             on = "l";
@@ -53,166 +111,92 @@
             run = "plugin smart-enter";
             desc = "Enter the child directory, or open the file";
           }
+          {
+            on = "F";
+            run = "plugin smart-filter";
+            desc = "Filter smartly";
+          }
+          {
+            on = ["g" "p"];
+            run = "plugin toggle-pane min-parent";
+            desc = "Minimize parent-directory pane";
+          }
+          {
+            on = ["g" "P"];
+            run = "plugin toggle-pane max-parent";
+            desc = "Maximize parent-directory pane";
+          }
+          {
+            on = ["g" "c"];
+            run = "plugin toggle-pane min-current";
+            desc = "Minimize current-directory pane";
+          }
+          {
+            on = ["g" "C"];
+            run = "plugin toggle-pane max-current";
+            desc = "Maximize current-directory pane";
+          }
+          {
+            on = ["g" "t"];
+            run = "plugin toggle-pane min-preview";
+            desc = "Minimize preview pane";
+          }
+          {
+            on = ["g" "T"];
+            run = "plugin toggle-pane max-preview";
+            desc = "Maximize preview pane";
+          }
         ];
         append_keymap = [
           {
-            on = ["g" "D"];
-            run = ''shell '${lib.getExe pkgs.xdragon} --on-top --and-exit --all $@' --confirm '';
-            desc = "Drag and drop";
-          }
-          {
             on = ["C" "z"];
-            run = "plugin ouch --args=zip";
+            run = "plugin ouch zip";
             desc = "Compress into .zip";
           }
           {
             on = ["C" "t"];
-            run = "plugin ouch --args=tar";
+            run = "plugin ouch tar";
             desc = "Compress into .tar";
           }
           {
             on = ["C" "g"];
-            run = "plugin ouch --args=tar.gz";
+            run = "plugin ouch tar.gz";
             desc = "Compress into .tar.gz";
           }
           {
             on = ["C" "7"];
-            run = "plugin ouch --args=7z";
+            run = "plugin ouch 7z";
             desc = "Compress into .7z";
           }
           {
             on = ["C" "x"];
-            run = "plugin ouch --args=tar.xz";
+            run = "plugin ouch tar.xz";
             desc = "Compress into .tar.xz";
           }
           {
             on = ["C" "b"];
-            run = "plugin ouch --args=tar.bz2";
+            run = "plugin ouch tar.bz2";
             desc = "Compress into .bz2";
           }
           {
             on = ["C" "l"];
-            run = "plugin ouch --args=tar.lz4";
+            run = "plugin ouch tar.lz4";
             desc = "Compress into .lz4";
           }
         ];
       };
     };
-    settings = {
-      plugin = {
-        prepend_fetchers = [
-          {
-            id = "git";
-            name = "*";
-            run = "git";
-          }
-          {
-            id = "git";
-            name = "*/";
-            run = "git";
-          }
-        ];
-        append_previewers = [
-          {
-            name = "*.csv";
-            run = "rich-preview";
-          }
-          {
-            name = "*.md";
-            run = "rich-preview";
-          }
-          {
-            name = "*.rst";
-            run = "rich-preview";
-          }
-          {
-            name = "*.ipynb";
-            run = "rich-preview";
-          }
-          {
-            name = "*.json";
-            run = "rich-preview";
-          }
-          {
-            name = "*.zip";
-            run = "ouch";
-          }
-          {
-            name = "*.7z";
-            run = "ouch";
-          }
-          {
-            name = "*.tar";
-            run = "ouch";
-          }
-          {
-            name = "*.tar.*";
-            run = "ouch";
-          }
-          {
-            name = "*.rar";
-            run = "ouch";
-          }
-          {
-            name = "*";
-            run = "hexyl";
-          }
-        ];
-      };
-      preview = {
-        max-height = 1000;
-        max-width = 1000;
-      };
-    };
-    plugins = let
-      yazi-rs = pkgs.fetchFromGitHub {
-        owner = "yazi-rs";
-        repo = "plugins";
-        rev = "02d18be03812415097e83c6a912924560e4cec6d";
-        hash = "sha256-1FZ8wcf2VVp6ZWY27vm1dUU1KAL32WwoYbNA/8RUAog=";
-      };
-    in {
-      chmod = builtins.path {path = "${yazi-rs}/chmod.yazi";};
-      diff = builtins.path {path = "${yazi-rs}/diff.yazi";};
-      full-border = builtins.path {path = "${yazi-rs}/full-border.yazi";};
-      git = builtins.path {path = "${yazi-rs}/git.yazi";};
-      hide-preview = builtins.path {path = "${yazi-rs}/hide-preview.yazi";};
-      max-preview = builtins.path {path = "${yazi-rs}/max-preview.yazi";};
-      smart-filter = builtins.path {path = "${yazi-rs}/smart-filter.yazi";};
-      smart-enter = builtins.path {path = "${yazi-rs}/smart-enter.yazi";};
-      hexyl = pkgs.fetchFromGitHub {
-        owner = "Reledia";
-        repo = "hexyl.yazi";
-        rev = "main";
-        hash = "sha256-nsnnL3GluKk/p1dQZTZ/RwQPlAmTBu9mQzHz1g7K0Ww=";
-      };
-      ouch = pkgs.fetchFromGitHub {
-        owner = "ndtoan96";
-        repo = "ouch.yazi";
-        rev = "main";
-        hash = "sha256-zLAaJrcZGNWlG2HjsZtN4u8JZAN+GLl2RtP9qCt3T74=";
-      };
-      rich-preview = pkgs.fetchFromGitHub {
-        owner = "AnirudhG07";
-        repo = "rich-preview.yazi";
-        rev = "main";
-        hash = "sha256-TwL0gIcDhp0hMnC4dPqaVWIXhghy977DmZ+yPoF/N98=";
-      };
-      what-size = pkgs.fetchFromGitHub {
-        owner = "pirafrank";
-        repo = "what-size.yazi";
-        rev = "main";
-        hash = "sha256-SDObD22u2XYF2BYKsdw9ZM+yJLH9xYTwSFRWIwMCi08=";
-      };
-    };
     initLua = ''
-      require("git"):setup()
       require("full-border"):setup { type = ui.Border.ROUNDED }
+      require("smart-enter"):setup { open_multi = true, }
+      require("git"):setup()
+      th.git = th.git or {}
+      th.git.untracked = ui.Style():fg("white")
+      th.git.ignored = ui.Style():fg("gray")
+      th.git.added = ui.Style():fg("green")
+      th.git.modified = ui.Style():fg("yellow")
+      th.git.deleted = ui.Style():fg("red")
+      th.git.updated = ui.Style():fg("blue")
     '';
   };
-  home.packages = [
-    pkgs.hexyl
-    pkgs.ouch
-    pkgs.rich-cli
-  ];
 }
